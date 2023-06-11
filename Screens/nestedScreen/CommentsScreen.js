1814c47e79d9263d0a1b530e45e80a20389fcd49
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-06-11 14:31:22 +0300

    +

--- Screens/nestedScreen/CommentsScreen.js
+++ Screens/nestedScreen/CommentsScreen.js
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function CommentsScreen() {
   return (
-    <View>
-      <Text>CommentsScreen</Text>
+    <View style={styles.container}>
+      <Text>Comments</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
